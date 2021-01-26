a2ace519d690c4cdd8755a6bf87919c24b67ea94
Liam Tyrie <liamtyrie@gmail.com>
2021-01-26 14:56:36 +0000

    Backend Almost Complete

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,118 @@
+import asyncHandler from 'express-async-handler'
+import Order from '../models/productModel.js'
+
+// CREATE NEW ORDER (PRIVATE)
+
+const addOrderItems = asyncHandler(async (req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        vat,
+        shippingPrice,
+        totalPrice,
+    } = req.body
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error('No order items')
+        return
+    } else {
+        const order = new Order({
+            orderItem s,
+            user: req.user._id,
+            shippingPrice,
+            paymentMethod,
+            itemsPrice,
+            vat,
+            shippingPrice,
+            totalPrice
+        })
+
+        const createdOrder = await order.save()
+
+        res.status(201).json(createdOrder)
+    }
+})
+
+// GET ORDER BY ID (PRIVATE)
+
+const getOrderById = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
+
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+// UPDATE ORDER TO PAID
+
+const updateOrderToPaid = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body._id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+// UPDATE ORDER TO DISPATCHED (PRIVATE/ADMIN)
+
+const updateOrderToDispatched = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isDispatched = true
+        order.dispatchedAt = Date.now()
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+// GET LOGGED IN USER ORDERS (PRIVATE)
+
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id})
+    res.json(orders)
+})
+
+// GET ALL ORDERS (PRIVATE/ADMIN)
+
+const getOrders = asyncHandler(async (req, res) => {
+     const orders = await Order.find({}).populate('user', 'id name')
+     res.json(orders)
+})
+
+export {
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDispatched,
+    getMyOrders,
+    getOrders
+}
---
+++ backend/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        name: 'Nike Air Zoom Pegasus 37',
+        image: [
+            {
+                1: '/images/nike_pegasus37_1.jpeg',
+                2: '/images/nike_pegasus37_2.jpeg',
+                3: '/images/nike_pegasus37_3.jpeg',
+                4: '/images/nike_pegasus37_4.jpeg',
+                5: '/images/nike_pegasus37_5.jpeg',
+                6: '/images/nike_pegasus37_6.jpeg',
+            }
+        ],
+        description: 
+        'Hit a new PB in these mens Air Zoom Pegasus 37 trainers from Nike. In an Off Noir colourway with hits of Blue Fury and Bright Crimson, these runners have lightweight mesh upper for a breathable feel. They feature a secure lace up fastening and are sat on a soft foam midsole for long-lasting cushioning and maximum responsiveness. With Nikes Air Zoom unit at the forefront for a smooth ride, these trainers are finished with a grippy rubber tread and the iconic Swoosh logo to the sidewalls.',
+        material: 'Textile & Synthetic Upper/Synthetic Sole',
+        brand: 'Nike',
+        category: 'running',
+        price: 105.00,
+        countInStock: 10,
+        rating: 0,
+        numReviews: 0,
+    },
+    
+]
